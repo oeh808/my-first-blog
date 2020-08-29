7d67772bb6e4a74d57e851b991b2286fe790b714
oeh808 <omarehababdelaziz8@gmail.com>
2020-08-29 23:54:02 +0300

    Changes made to file structure

--- functional_tests.py
+++ functional_tests.py
@@ -9,20 +9,14 @@ class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
+
     def tearDown(self):
         self.browser.quit()
 
     def test_cv_site_exists(self):
         #The user starts by finding a link to a cv site
-        self.browser.get('http://127.0.0.1:8000/cv')
+        self.browser.get('http://127.0.0.1:8000/cv_view')
         self.assertNotIn('Page not found',self.browser.title)
-
-
-#The user sees a link to the cv web page
-
-#The user clicks the link and gets redirected to the CV website
-#The user is then able to see the details of the cv before them
-#The user should be able to see an image of the CV holder
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
