f3334c1017bfd6158cbc1a0dfde2fa5e8fad5593
oeh808 <omarehababdelaziz8@gmail.com>
2020-08-29 23:15:14 +0300

    Functional_tests now functional (badum-tss)

--- functional_tests.py
+++ functional_tests.py
@@ -1,22 +1,28 @@
 from selenium import webdriver
 import unittest
+from django.template.loader import render_to_string
+from selenium.webdriver.common.keys import Keys
+import time
 #Here are the comments detailing how a user should use the site
+
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
-        self.browser = webdriver.FireFox()
+        self.browser = webdriver.Firefox()
+
     def tearDown(self):
         self.browser.quit()
 
-    def can_see_cv_button(self):
-        #The user starts by opening the main site (Blog website)
-        self.browser.get('http://localhost:8000')
-
-        self.fail('Finish the test!')
-        #The user sees a link to the cv web page
+    def test_cv_site_exists(self):
+        #The user starts by finding a link to a cv site
+        self.browser.get('http://127.0.0.1:8000/cv')
+        self.assertNotIn('Page not found',self.browser.title)
 
 
-        #The user clicks the link and gets redirected to the CV website
-        #The user is then able to see the details of the cv before them
-        #The user should be able to see an image of the CV holder
+#The user sees a link to the cv web page
 
-browser.quit()
+#The user clicks the link and gets redirected to the CV website
+#The user is then able to see the details of the cv before them
+#The user should be able to see an image of the CV holder
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
